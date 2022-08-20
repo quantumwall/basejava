974dccf843d3567e64936da82d67e96a0e5e4549
quantumwall <liberiamor@gmail.com>
2022-08-20 12:37:19 +0300

    change decrement in delete();

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -36,8 +36,8 @@ public class ArrayStorage {
     void delete(String uuid) {
         for(int i = 0; i < size; i++) {
             if(storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size - 1];
-                storage[--size] = null;
+                storage[i] = storage[--size];
+                storage[size] = null;
                 break;
             }
         }
