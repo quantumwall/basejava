fb9ae2bae3a85ffa4c14ecdeafef95acc4df523c
quantumwall <liberiamor@gmail.com>
2022-11-13 17:32:46 +0300

    sql query to create text and list sections;

--- config/init_db.sql
+++ config/init_db.sql
@@ -12,3 +12,21 @@ CREATE TABLE contact
     value TEXT NOT NULL,
     UNIQUE (resume_id, type)
 );
+
+CREATE TABLE text_section
+(
+    id SERIAL PRIMARY KEY,
+    resume_id VARCHAR(36) REFERENCES resume ON DELETE CASCADE,
+    type TEXT NOT NULL,
+    value TEXT NOT NULL,
+    UNIQUE(resume_id, type)
+);
+
+CREATE TABLE list_section
+(
+    id SERIAL PRIMARY KEY,
+    resume_id VARCHAR(36) REFERENCES resume ON DELETE CASCADE,
+    type TEXT NOT NULL,
+    value TEXT NOT NULL,
+    UNIQUE(resume_id, type)
+);
